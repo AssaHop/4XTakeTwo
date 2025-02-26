3e8b70b6393c55c1dd3aea984f9e693930af7e0e
AssaHop <mytempdropbox55@gmail.com>
2025-02-26 22:55:00 +0000

    add setupui

---
+++ src/ui/setup.js
@@ -0,0 +1,8 @@
+function setupUI() {
+    // Примерная реализация функции setupUI
+    console.log("UI setup initialized");
+    // Добавление любых других необходимых действий по настройке UI
+}
+
+export { setupUI };
+
